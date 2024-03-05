c582ac9516499afc422dc2a75dd4477bd66684db
Parasamit <Amitbahadur@parastechnologies.com>
2024-03-05 07:54:40 +0530

    map screen btn color update

--- lib/app/google_maps/google_map_screen.dart
+++ lib/app/google_maps/google_map_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:northshore_nanny_flutter/app/google_maps/google_map_controller.dart';
+import 'package:northshore_nanny_flutter/app/res/theme/colors.dart';
 import 'package:northshore_nanny_flutter/app/res/theme/styles.dart';
 import 'package:northshore_nanny_flutter/app/widgets/custom_button.dart';
 
@@ -43,6 +44,7 @@ class CurrentLocationGoogleMap extends StatelessWidget {
         floatingActionButton: CustomButton(
           titleStyle: AppStyles.navyBlue15UbW600,
           title: "Done",
+          backGroundColor: AppColors.navyBlue3288DE,
           onTap: () {
             googleMapViewController.saveLocationCoordinates();
           },
