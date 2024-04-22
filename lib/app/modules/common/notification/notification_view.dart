8349eaaadabb62e6a32a14bba8c372f9a75c15b0
Parasjaskaran <157676404+Parasjaskaran@users.noreply.github.com>
2024-04-22 16:57:47 +0530

    fix the notification loading issue

--- lib/app/modules/common/notification/notification_view.dart
+++ lib/app/modules/common/notification/notification_view.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:northshore_nanny_flutter/app/modules/common/dashboard_bottom/dashboard_bottom_controller.dart';
 import 'package:northshore_nanny_flutter/app/modules/common/notification/notification_controller.dart';
 import 'package:northshore_nanny_flutter/app/modules/nanny/nanny_views/nanny_booking_detail/nanny_booking_detail_binding.dart';
 import 'package:northshore_nanny_flutter/app/res/theme/styles.dart';
@@ -23,9 +22,7 @@ class NotificationView extends StatelessWidget {
   @override
   Widget build(BuildContext context) => GetBuilder<NotificationController>(
         init: NotificationController(),
-        initState: (_){
-
-        },
+        initState: (_) {},
         builder: (notificationController) => Scaffold(
           appBar: CustomAppbarWidget(
             addBackButton: false,
@@ -38,132 +35,129 @@ class NotificationView extends StatelessWidget {
           body: Padding(
             padding: Dimens.edgeInsets16,
             child: SingleChildScrollView(
-              child: notificationController.notificationListModel?.data ==
-                          null &&
-                      notificationController
-                              .notificationListModel?.data?.isEmpty ==
-                          true && !notificationController.isNotificationLoading.value
-                  ?Center(
-                child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      SizedBox(
-                        height: Get.height / 3,
-                      ),
-                      AppText(
-                        text: TranslationKeys.noResultFound.tr,
-                        style: AppStyles.pdNavyBlue20W600,
-                        textAlign: TextAlign.center,
-                      ),
-                    ]),
-              )
-                  : Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: List.generate(
-                  notificationController
-                      .notificationListModel?.data?.length ??
-                      0,
-                      (index) => InkWell(
-                    splashColor: Colors.transparent,
-                    onTap: () {
-                      var loginType =
-                      Storage.getValue(StringConstants.loginType);
-
-                      /// used to read the notification.
-                      notificationController
-                          .postNotificationRead(
-                          notificationId: notificationController
-                              .notificationListModel
-                              ?.data?[index]
-                              .notificationId ??
-                              0)
-                          .then((value) {
-                        Get.find<DashboardBottomController>()
-                            .getNotificationCount();
-                      });
-                      if (loginType == StringConstants.customer) {
-                        if (!Get.isRegistered<
-                            BookingDetailController>()) {
-                          BookingDetailBinding().dependencies();
-                        }
-                        var customerBookingController =
-                        Get.find<BookingDetailController>();
-
-                        /// api used to Store  booking Status type.
-                        customerBookingController.typeOfBooking(
-                            bookingStatus: notificationController
-                                .notificationListModel
-                                ?.data?[index]
-                                .bookingStatus);
-
-                        /// api used to get data of booking.
-                        customerBookingController.getBookingDataById(
-                          bookingId: notificationController
-                              .notificationListModel
-                              ?.data?[index]
-                              .bookingId,
-                        );
-
-                        /// going to route
-                        RouteManagement.goToCustomerBookingDetailView();
-                      } else if (loginType == StringConstants.nanny) {
-                        if (!Get.isRegistered<
-                            NannyBookingDetailController>()) {
-                          NannyBookingDetailBinding().dependencies();
-                        }
-
-                        var nannyBookingController =
-                        Get.find<NannyBookingDetailController>();
-
-                        /// used to store the type of booking
-                        nannyBookingController.typeOfBooking(
-                            bookingStatus: notificationController
-                                .notificationListModel
-                                ?.data?[index]
-                                .bookingStatus ??
-                                0);
-
-                        /// api used to get details of booking
-                        nannyBookingController.getBookingDetailOfCustomer(
-                          bookingId: notificationController
-                              .notificationListModel
-                              ?.data?[index]
-                              .bookingId ??
+                child: notificationController.notificationListModel?.data !=
+                            null &&
+                        !notificationController.isNotificationLoading.value &&
+                        notificationController
+                                .notificationListModel?.data?.isEmpty ==
+                            true
+                    ? Center(
+                        child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              SizedBox(
+                                height: Get.height / 3,
+                              ),
+                              AppText(
+                                text: TranslationKeys.noResultFound.tr,
+                                style: AppStyles.pdNavyBlue20W600,
+                                textAlign: TextAlign.center,
+                              ),
+                            ]),
+                      )
+                    : Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: List.generate(
+                          notificationController
+                                  .notificationListModel?.data?.length ??
                               0,
-                        );
+                          (index) => InkWell(
+                            splashColor: Colors.transparent,
+                            onTap: () {
+                              var loginType =
+                                  Storage.getValue(StringConstants.loginType);
 
-                        /// going to route
-                        RouteManagement.goToNannyBookingView();
-                      }
-                      notificationController.getNotificationList();
-                    },
-                    child: CustomNotificationTile(
-                      notificationHeading: notificationController
-                          .notificationListModel
-                          ?.data?[index]
-                          .title ??
-                          '',
-                      notificationSubHeading: notificationController
-                          .notificationListModel
-                          ?.data?[index]
-                          .description ??
-                          '',
-                      notificationTime: DateHelper.getDateTimeAgo(
-                          notificationController.notificationListModel
-                              ?.data?[index].createdOn
-                              .toString() ??
-                              ''),
-                      isNotificationRead: notificationController
-                          .notificationListModel
-                          ?.data?[index]
-                          .isRead ??
-                          false,
-                    ),
-                  ),
-                ),
-              )
-            ),
+                              /// used to read the notification.
+                              notificationController
+                                  .postNotificationRead(
+                                      notificationId: notificationController
+                                              .notificationListModel
+                                              ?.data?[index]
+                                              .notificationId ??
+                                          0);
+                              if (loginType == StringConstants.customer) {
+                                if (!Get.isRegistered<
+                                    BookingDetailController>()) {
+                                  BookingDetailBinding().dependencies();
+                                }
+                                var customerBookingController =
+                                    Get.find<BookingDetailController>();
+
+                                /// api used to Store  booking Status type.
+                                customerBookingController.typeOfBooking(
+                                    bookingStatus: notificationController
+                                        .notificationListModel
+                                        ?.data?[index]
+                                        .bookingStatus);
+
+                                /// api used to get data of booking.
+                                customerBookingController.getBookingDataById(
+                                  bookingId: notificationController
+                                      .notificationListModel
+                                      ?.data?[index]
+                                      .bookingId,
+                                );
+
+                                /// going to route
+                                RouteManagement.goToCustomerBookingDetailView();
+                              } else if (loginType == StringConstants.nanny) {
+                                if (!Get.isRegistered<
+                                    NannyBookingDetailController>()) {
+                                  NannyBookingDetailBinding().dependencies();
+                                }
+
+                                var nannyBookingController =
+                                    Get.find<NannyBookingDetailController>();
+
+                                /// used to store the type of booking
+                                nannyBookingController.typeOfBooking(
+                                    bookingStatus: notificationController
+                                            .notificationListModel
+                                            ?.data?[index]
+                                            .bookingStatus ??
+                                        0);
+
+                                /// api used to get details of booking
+                                nannyBookingController
+                                    .getBookingDetailOfCustomer(
+                                  bookingId: notificationController
+                                          .notificationListModel
+                                          ?.data?[index]
+                                          .bookingId ??
+                                      0,
+                                );
+
+                                /// going to route
+                                RouteManagement.goToNannyBookingView();
+                              }
+                              notificationController.getNotificationList();
+                            },
+                            child: CustomNotificationTile(
+                              notificationHeading: notificationController
+                                      .notificationListModel
+                                      ?.data?[index]
+                                      .title ??
+                                  '',
+                              notificationSubHeading: notificationController
+                                      .notificationListModel
+                                      ?.data?[index]
+                                      .description ??
+                                  '',
+                              notificationTime: DateHelper.getDateTimeAgo(
+                                  notificationController.notificationListModel
+                                          ?.data?[index].createdOn
+                                          .toString() ??
+                                      ''),
+                              isNotificationRead: notificationController
+                                      .notificationListModel
+                                      ?.data?[index]
+                                      .isRead ??
+                                  false,
+                            ),
+                          ),
+                        ),
+                      )),
           ),
         ),
       );
