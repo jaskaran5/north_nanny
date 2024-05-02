7e6ab772bd9b40124b51ea970fca71cf2205df9f
Parasjaskaran <157676404+Parasjaskaran@users.noreply.github.com>
2024-05-02 11:17:00 +0530

    fix the format in recent chat view

--- lib/app/widgets/custom_recant_chat_list.dart
+++ lib/app/widgets/custom_recant_chat_list.dart
@@ -25,97 +25,96 @@ class CustomRecentChatListTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetBuilder<RecentChatController>(
-        init: RecentChatController(),
-        builder: (controller) {
-          return Padding(
-            padding: Dimens.edgeInsets12,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Stack(children: [
-                  (chatData!.image!.isNotEmpty)
-                      ? CustomCacheNetworkImage(
-                          img: chatData!.image!,
-                          size: Dimens.fifty,
-                          imageRadius: Dimens.fifty)
-                      : ClipRRect(
-                          borderRadius: BorderRadius.circular(30),
-                          child: Image.asset(
-                            Assets.imagesUserAvatar,
-                            height: Dimens.fifty,
-                            width: Dimens.fifty,
-                            fit: BoxFit.cover,
-                          ),
-                        ),
-                  Positioned(
-                    bottom: 2,
-                    right: 3,
-                    child: Container(
-                      decoration: BoxDecoration(
-                          color: AppColors.primaryColor,
-                          borderRadius: BorderRadius.circular(Dimens.ten)),
-                      child: Icon(
-                        Icons.circle,
-                        color: chatData!.isOnline!
-                            ? AppColors.onlineColor
-                            : Colors.red,
-                        size: Dimens.ten,
+      init: RecentChatController(),
+      builder: (controller) => Padding(
+        padding: Dimens.edgeInsets12,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Stack(children: [
+              (chatData!.image!.isNotEmpty)
+                  ? CustomCacheNetworkImage(
+                      img: chatData!.image!,
+                      size: Dimens.fifty,
+                      imageRadius: Dimens.fifty)
+                  : ClipRRect(
+                      borderRadius: BorderRadius.circular(30),
+                      child: Image.asset(
+                        Assets.imagesUserAvatar,
+                        height: Dimens.fifty,
+                        width: Dimens.fifty,
+                        fit: BoxFit.cover,
                       ),
                     ),
-                  )
-                ]),
-                Dimens.boxWidth10,
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    SizedBox(
-                      width: Get.width * .5,
-                      child: Text(
-                        chatData?.fullName ?? '',
-                        style: chatData?.unreadMessageCount == 0
-                            ? AppStyles.ubBlack14W700
-                            : AppStyles.ubNavyBlue14W700,
-                        maxLines: 1,
-                        overflow: TextOverflow.ellipsis,
-                      ),
-                    ),
-                    Dimens.boxHeight8,
-                    chatData!.isLastMessageImage!
-                        ? _buildFileWidget(fileType: chatData?.fileType ?? '')
-                        : SizedBox(
-                            width: Get.width * .5,
-                            child: Text(
-                              chatData?.lastMessage ?? '',
-                              style: chatData?.unreadMessageCount == 0
-                                  ? AppStyles.ubGrey12W500
-                                  : AppStyles.ubBlack12W600,
-                              overflow: TextOverflow.ellipsis,
-                              maxLines: 1,
-                            ),
-                          ),
-                  ],
+              Positioned(
+                bottom: 2,
+                right: 3,
+                child: Container(
+                  decoration: BoxDecoration(
+                      color: AppColors.primaryColor,
+                      borderRadius: BorderRadius.circular(Dimens.ten)),
+                  child: Icon(
+                    Icons.circle,
+                    color: chatData!.isOnline!
+                        ? AppColors.onlineColor
+                        : Colors.red,
+                    size: Dimens.ten,
+                  ),
                 ),
-                const Spacer(),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    Text(
-                      DateHelper.getDateTimeAgo(chatData!.dateTime.toString()),
-                      style: AppStyles.ubChatTimeColor12W400,
-                    ),
-                    Dimens.boxHeight8,
-                    chatData?.unreadMessageCount != 0
-                        ? CustomNewMessageCountIcon(
-                            count: chatData!.unreadMessageCount.toString(),
-                          )
-                        : const SizedBox()
-                  ],
-                )
+              )
+            ]),
+            Dimens.boxWidth10,
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  width: Get.width * .5,
+                  child: Text(
+                    chatData?.fullName ?? '',
+                    style: chatData?.unreadMessageCount == 0
+                        ? AppStyles.ubBlack14W700
+                        : AppStyles.ubNavyBlue14W700,
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                ),
+                Dimens.boxHeight8,
+                chatData?.isLastMessageImage == true
+                    ? _buildFileWidget(fileType: chatData?.fileType ?? '')
+                    : SizedBox(
+                        width: Get.width * .5,
+                        child: Text(
+                          chatData?.lastMessage ?? '',
+                          style: chatData?.unreadMessageCount == 0
+                              ? AppStyles.ubGrey12W500
+                              : AppStyles.ubBlack12W600,
+                          overflow: TextOverflow.ellipsis,
+                          maxLines: 1,
+                        ),
+                      ),
               ],
             ),
-          );
-        });
+            const Spacer(),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Text(
+                  DateHelper.getDateTimeAgo(chatData!.dateTime.toString()),
+                  style: AppStyles.ubChatTimeColor12W400,
+                ),
+                Dimens.boxHeight8,
+                chatData?.unreadMessageCount != 0
+                    ? CustomNewMessageCountIcon(
+                        count: chatData!.unreadMessageCount.toString(),
+                      )
+                    : const SizedBox()
+              ],
+            )
+          ],
+        ),
+      ),
+    );
   }
 
   Widget _buildFileWidget({fileType}) {
